1b6e91cf060bbe451e993fc3fb2ef5bbe55047d0
Bri98E <encina.brian.hector@gmail.com>
2022-12-17 10:58:06 -0300

    proyecto2

--- main.js
+++ main.js
@@ -1,134 +1,109 @@
-const comprarProductos = () => {
-    let producto = '';
-    let cantidad = 0;
-    let precio = 0;
-    let totalCompra = 0;
-    let seguirComprando = false;
+const carrito = [];
+
+const ordenarMenorMayor = () => {
+    productos.sort((a, b) => a.precio - b.precio)
+    mostrarListaOrdenada()
+}
+
+const ordenarMayorMenor = () => {
+    productos.sort((a, b) => b.precio - a.precio)
+    mostrarListaOrdenada()
+}
+
+const mostrarListaOrdenada = () => {
+    const listaOrdenada = productos.map(producto => {
+        return '- '+producto.nombre+' $'+producto.precio
+    });
+    alert('Lista de precios:'+'\n\n'+listaOrdenada.join('\n'))
+    comprarProductos(listaOrdenada)
+};
+
+const comprarProductos = (listaDeProductos) => {
+    let otroProducto = false;
+    let productoNombre = '';
+    let productoCantidad = 0;
 
     do {
-        producto = prompt ("¿Querés comprar Baño, Casita, Raton o Collar?");
-        cantidad = parseInt(prompt ("¿Cuántos querés comprar?"));
+        productoNombre = prompt('¿Que producto desea comprar?'+'\n\n'+listaDeProductos.join('\n'));
+        productoCantidad = parseInt(prompt('¿Cuántos querés comprar?'));
 
-        let cantidadValidada = validarCantidad(cantidad);
+        const producto = productos.find(producto => producto.nombre.toLowerCase() === productoNombre.toLowerCase())
 
-    switch (producto) {
-        case "Baño":
-            precio = 1700;
-            break;
-        case "Casita":
-            precio = 5000;
-            break;
-        case "Raton":
-            precio = 350;
-            break;
-        case "Collar":
-            precio = 600;
-            break;
-        default:
-            alert("Alguno de los datos ingresados no es correcto");
-            precio= 0;
-            cantidad= 0;
-    }
-
-    totalCompra += precio * cantidadValidada;
-
-    
-    seguirComprando = confirm("¿Querés agregar otro producto?");
-
-    } while (seguirComprando)
-
-    const totalConDescuento = aplicarDescuento(totalCompra);
-    const totalConEnvio = calcularEnvio(totalConDescuento);
-
-    return totalConEnvio;
-}
-
-const validarCantidad = (cantidad) => {
-    while (Number.isNaN(cantidad) || cantidad === 0) {
-        if (cantidad !== 0) {
-            alert('Debe agregar un número.')
+        if (producto) {
+            agregarAlCarrito(producto, producto.id, productoCantidad)
         } else {
-            alert('Debe agregar una cantidad de producto.')
+            alert('El producto no se encuentra en el catálogo')
         }
-        cantidad = parseInt(prompt("¿Cuántos querés comprar?"))
-    }
 
-    return cantidad;
-};
-const aplicarDescuento = (totalCompra) => {
-    let totalConDescuento = 0;
+        otroProducto = confirm('¿Desea agregar otro producto?')
+    } while (otroProducto);
 
-    if (totalCompra >= 5000) {
-        totalConDescuento = totalCompra * 0.90;
-        return totalConDescuento;
-    } else {
-        return totalCompra;
-    }
-}
-const calcularEnvio = (totalCompra) => {
-    let tieneEnvioADomicilio = false;
-
-    tieneEnvioADomicilio = confirm("¿Querés envío a domicilio?");
-
-    if (tieneEnvioADomicilio && totalCompra >= 2000) {
-      alert("Tenés envio gratis. El total de tu compra es $" + totalCompra);
-    } else if (tieneEnvioADomicilio && totalCompra < 2000 && totalCompra !== 0) {
-      totalCompra += 700;
-      alert("El envío cuesta $700. El total de tu compra es $" + totalCompra);
-    } else {
-      alert("El total de tu compra es $" + totalCompra);
-    }
-
-    return totalCompra;
+   confirmarCompra();
 };
 
-const calcularCantidadDeCuotas = () => {
-    let cuotas = 0;
-    let tieneCuotas = false;
+const agregarAlCarrito = (producto, productoId, productoCantidad) => {
+    const productoRepetido = carrito.find(producto => producto.id === productoId);
+    if (productoRepetido) {
+        productoRepetido.cantidad += productoCantidad
+    } else {
+        producto.cantidad += productoCantidad
+        carrito.push(producto)
+    }
+};
 
-    tieneCuotas = confirm('¿Queres pagar en cuotas?');
-
-    if (tieneCuotas) {
-        cuotas = parseInt(prompt('¿En cuantas cuotas queres pagar?'));
-        if (cuotas === 0) {
-            cuotas = 1;
-        } else if (Number.isNaN(cuotas)) {
-            calcularCantidadDeCuotas();
+const eliminarProductoCarrito = (productoNombre) => {
+    carrito.forEach((producto, index) => {
+        if (producto.nombre.toLowerCase() === productoNombre) {
+            if (producto.cantidad > 1) {
+                producto.cantidad--
+            } else {
+                carrito.splice(index, 1)
+            }
         }
+    })
+    confirmarCompra()
+};
+
+const confirmarCompra = () => {
+    const listaProductos = carrito.map(producto => {
+        return '- '+producto.nombre+' | Cantidad: '+producto.cantidad
+    });
+
+    const confirmar = confirm('Checkout: '
+        +'\n\n'+listaProductos.join('\n')
+        +'\n\nPara continuar presione "Aceptar" sino "Cancelar" para eliminar productos del carrito'
+    );
+
+    if (confirmar) {
+        finalizarCompra(listaProductos)
     } else {
-        cuotas = 1
+        const productoAEliminar = prompt('Ingrese el nombre del producto a eliminar:');
+        eliminarProductoCarrito(productoAEliminar)
     }
-
-    return cuotas;
 };
 
-const calcularIntereses = (cuotas) => {
-    let tasa = 12.3;
-    let sinIntereses = 0;
-    let tasaTotal = 0;
-    let interesesTotales = 0;
+const finalizarCompra = (listaProductos) => {
+    const cantidadTotal = carrito.reduce((acc, item) => acc + item.cantidad, 0)
+    const precioTotal = carrito.reduce((acc, item) => acc + (item.precio * item.cantidad), 0)
+    alert('Detalle de su compra:'
+        +'\n\n'+listaProductos.join('\n')
+        +'\n\nTotal de productos: '+cantidadTotal
+        +'\n\nEl total de la compra es: '+precioTotal
+        +'\n\nGracias por su compra!'
+    )
+};
 
-    if (cuotas === 1) {
-        return sinIntereses;
+const comprar = () => {
+    const productosBaratos = confirm('¿Querés ordenar la lista de productos del más barato al más caro?');
+
+    if (productosBaratos) {
+        ordenarMenorMayor();
     } else {
-        tasaTotal = tasa + cuotas * 0.2;
-        interesesTotales = tasaTotal * cuotas;
-        return interesesTotales;
+        ordenarMayorMenor();
     }
 };
 
-const calcularTotalAPagar = (totalCompra, cuotas, intereses) => {
-    totalCompra = (totalCompra + intereses);
-    let valorCuota = totalCompra / cuotas;
-    alert('El total a pagar es $'+totalCompra+' en '+cuotas+' cuotas de $'+valorCuota.toFixed(2))
-};
-
-const totalCompra = comprarProductos();
-const cuotas = calcularCantidadDeCuotas();
-const intereses = calcularIntereses(cuotas);
-
-calcularTotalAPagar(totalCompra, cuotas, intereses);
-
+comprar();
 
 
 
---
+++ stock.js
@@ -0,0 +1,51 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Collar",
+        tipo: "Accesorios",
+        descripcion: "Collar para felinos",
+        precio: 1200,
+        tamaño: "Universal",
+        cantidad:0
+    },
+
+    {
+        id: 2,
+        nombre: "Casita",
+        tipo: "Accesorios",
+        descripcion: "Casa para perros",
+        precio: 10000,
+        tamaño: "Mediano",
+        cantidad:0
+    },
+
+    {
+        id: 3,
+        nombre: "Raton",
+        tipo: "juguetes",
+        descripcion: "Raton de juguete para gatos",
+        precio: 300,
+        tamaño: "chico",
+        cantidad:0
+    },
+
+    {
+        id: 4,
+        nombre: "Baño felinos",
+        tipo: "Higiene",
+        descripcion: "Baño para gatos",
+        precio: 3000,
+        tamaño: "grande",
+        cantidad:0
+    },
+
+    {
+        id: 5,
+        nombre: "Arnes",
+        tipo: "Accesorios",
+        descripcion: "Arnes para perros",
+        precio: 2000,
+        tamaño: "Mediano",
+        cantidad:0
+    },
+]
